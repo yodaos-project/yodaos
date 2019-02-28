6e3e4c46ad72fe98590084bc22174456d2baeda2
Yorkie Liu <yorkiefixer@gmail.com>
2019-02-28 16:45:38 +0800

    update script

---
+++ tools/ci_script.sh
@@ -0,0 +1,7 @@
+REPO=./tools/repo
+
+$REPO init \
+  -u https://github.com/yodaos-project/yodaos.git \
+  -m manifest.xml \
+  --repo-url=http://openai-corp.rokid.com/tools/repo \
+  --no-repo-verify
