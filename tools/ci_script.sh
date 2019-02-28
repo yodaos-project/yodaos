b1153554c8af9020fbfb7c42627ff4228ed53d70
circle ci <circleci@rokid.com>
2019-02-28 17:03:37 +0800

    fix the repo config command

--- tools/ci_script.sh
+++ tools/ci_script.sh
@@ -4,12 +4,11 @@ REPO=$CWD/tools/repo
 mkdir -p yodaos-source
 cd yodaos-source
 
-git config user.email "circleci@rokid.com"
-git config user.name "circle ci"
+git config --global user.email "circleci@rokid.com"
+git config --global user.name "circle ci"
 
 $REPO init \
   -u https://github.com/yodaos-project/yodaos.git \
   -m manifest.xml \
-  --config-name=circleci \
   --repo-url=http://openai-corp.rokid.com/tools/repo \
   --no-repo-verify
