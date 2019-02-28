f299ada01592a664a9090146efd6e025b3752d35
Yorkie Liu <yorkiefixer@gmail.com>
2019-02-28 16:54:53 +0800

    set default git config identity

--- tools/ci_script.sh
+++ tools/ci_script.sh
@@ -4,6 +4,9 @@ REPO=$CWD/tools/repo
 mkdir -p yodaos-source
 cd yodaos-source
 
+git config user.email "circleci@rokid.com"
+git config user.name "circle ci"
+
 $REPO init \
   -u https://github.com/yodaos-project/yodaos.git \
   -m manifest.xml \
