4e18a8f17bfc13825850743c96fb9651142315e8
circle ci <circleci@rokid.com>
2019-05-27 13:13:24 +0800

    add Dockerfile for downloading source

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:alpine
+
+# Install and config Supervisor
+RUN apk add python make bash git openssh
+
+# prepare sync tool
+COPY tools/repo /usr/bin/
+COPY tools/repo-username /usr/bin/
+
+# prepare yodaos codebase
+RUN mkdir -p /opt/codebase/
+COPY manifest.xml /opt/codebase/
+COPY manifests /opt/codebase/
+
+# Add a startup script
+WORKDIR /opt/codebase/
+ADD ./tools/download.sh /download.sh
+RUN chmod 755 /download.sh
+RUN chown -R node:0 /opt/codebase
+
+USER node
+CMD ["/download.sh"]
---
+++ tools/download.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cat /etc/passwd
+
+# verify sshkeys
+ssh -tt -o StrictHostKeyChecking=no -p 29418 $ROKID_USERNAME@openai-corp.rokid.com ""
+
+git config --global color.ui false
+git config --global user.email "yodaos@rokid.com"
+git config --global user.name "yodaos"
+
+repo init \
+  -u https://github.com/yodaos-project/yodaos.git \
+  -m manifest.xml \
+  --repo-url=http://openai-corp.rokid.com/tools/repo \
+  --no-repo-verify
+
+ls -la ./
+repo-username -u $ROKID_USERNAME
+repo sync
