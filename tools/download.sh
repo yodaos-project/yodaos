ea4df5592f7d23dc79483d2d8eec1c1b1767a0fe
circle ci <circleci@rokid.com>
2019-05-27 15:53:29 +0800

    remove chown commands

--- tools/download.sh
+++ tools/download.sh
@@ -2,9 +2,6 @@
 
 set -x
 
-chown -R node:$HOST_UID /opt/codebase
-ls -la /home/
-
 # verify sshkeys
 ssh -tt -o StrictHostKeyChecking=no -p 29418 $ROKID_USERNAME@openai-corp.rokid.com ""
 
@@ -18,6 +15,5 @@ repo init \
   --repo-url=http://openai-corp.rokid.com/tools/repo \
   --no-repo-verify
 
-ls -la ./
 repo-username -u $ROKID_USERNAME
 repo sync
